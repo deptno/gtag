34b2eb4518811220913406b074c5b649f199a71b
deptno <deptno@gmail.com>
2020-08-28 16:08:18 +0900

    refactor: gtag-react, remove react dependency

--- packages/gtag-react/src/Gtag.tsx
+++ packages/gtag-react/src/Gtag.tsx
@@ -1,16 +1,12 @@
-import {FunctionComponent} from 'react'
-
-export const GTag: FunctionComponent<Props> = props => {
-  const {id} = props
-
+export function GTag(props: Props) {
   return (
     <>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`}/>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${props.id}`}/>
       <script dangerouslySetInnerHTML={{
         __html: `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', '${id}');`,
+gtag('config', '${props.id}');`,
       }}/>
     </>
   )
