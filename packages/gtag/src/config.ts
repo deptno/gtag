99fc2e8538868a1b425ac2422697cac97b7b8e35
deptno <deptno@gmail.com>
2020-08-31 12:02:46 +0900

    lib: 0.1.6 fix: set, config argument overloading

--- packages/gtag/src/config.ts
+++ packages/gtag/src/config.ts
@@ -2,6 +2,7 @@ import Gtag from '../gtag'
 
 export function config(config: '[AW_CONVERSION_ID]' | string, params: Gtag.ControlParams): void
 export function config(config: '[GA_MEASUREMENT_ID]' | string, params: Gtag.ConfigGaMeasurementIdParams): void
-export function config(config: Gtag.ConfigType, params: Gtag.ControlParams|Gtag.ConfigGaMeasurementIdParams) {
+export function config(config: '[AW_CONVERSION_ID]'|'[GA_MEASUREMENT_ID]' | string): void
+export function config(config: Gtag.ConfigType, params?: Gtag.ControlParams|Gtag.ConfigGaMeasurementIdParams) {
   window.gtag('config', config, params)
 }
--- packages/gtag/src/set.ts
+++ packages/gtag/src/set.ts
@@ -2,6 +2,7 @@ import Gtag from '../gtag'
 
 export function set(config: '[AW_CONVERSION_ID]' | string, params: Gtag.ControlParams): void
 export function set(config: '[GA_MEASUREMENT_ID]' | string, params: Gtag.ConfigGaMeasurementIdParams): void
-export function set(config: Gtag.ConfigType, params: Gtag.ControlParams|Gtag.ConfigGaMeasurementIdParams) {
+export function set(config: '[AW_CONVERSION_ID]'|'[GA_MEASUREMENT_ID]' | string): void
+export function set(config: Gtag.ConfigType, params?: Gtag.ControlParams|Gtag.ConfigGaMeasurementIdParams) {
   window.gtag('set', config, params)
 }
